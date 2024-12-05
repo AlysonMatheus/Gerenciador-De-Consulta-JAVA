11346359c42bb73d3fca0139df1fc8c058171359
AlysonMatheus <alyson2000@live.com>
2024-12-05 18:46:15 -0300

    Update MedicoController.java

--- Consulta/src/main/java/com/example/Consulta/Controller/MedicoController.java
+++ Consulta/src/main/java/com/example/Consulta/Controller/MedicoController.java
@@ -1,5 +1,23 @@
 package com.example.Consulta.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Consulta.Model.Medico;
+;
+
 public class MedicoController {
-    
+     @GetMapping("/cadastromedico")
+    public String showForm(Medico model) {
+        model.addAttribute("paciente", new Medico());  // Cria um novo objeto medico
+        return "cadastrarmedico";  // Retorna a página Thymeleaf
+    }
+
+    @PostMapping("/salvarmedico")
+    public String salvarMedico(@ModelAttribute Medico medico) {
+       
+        return "redirect:/paciente/cadastrado";  
+}
 }
